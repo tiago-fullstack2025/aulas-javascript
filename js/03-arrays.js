a3ff8372c6a541a1cba5d1441b90ac3a9973c361
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-07-30 17:30:11 -0300

    Update 03-arrays.js

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,16 @@
 'use strict';
 
+// Declarando um array (vetor)
+let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"];
+
+// Se quisermos visualizar a estrutura do array inteiro:
+// console.log(vocalistas);
+
+// Acessando um determinando elemento através do índice
+console.log(vocalistas[0]);
+
+// O David Gilmour está em turnê tocando Pink Floyd
+console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
+
+
 
