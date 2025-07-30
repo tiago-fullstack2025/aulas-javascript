819bccfd77dda5be690fcb2d9c9e42dd0bb5bbd3
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-07-30 16:39:58 -0300

    Corrige exercicios, trata strings

---
+++ js/02-meu-script.js
@@ -0,0 +1,22 @@
+/* Exercícios */
+
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+let uc = 2;
+
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+let competencia = "Front-End com JavaScript";
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+console.log(`Estamos na UC${uc} estudando ${competencia}.`);
+
+// ERRADO!
+//console.log("Gosto "muito" de paçoca!");
+
+// CORRETO! Aspas simples como comando, e duplas como texto
+console.log('Gosto "muito" de paçoca!');
+
+// CORRETO! Aspas duplas como comando, e simples como texto
+console.log("Gosto 'muito' de paçoca!");
+
+// CORRETO! Escapando as aspas como texto
+console.log("Gosto \"muito\" de paçoca!");
