c9689cc043fa0baa0478f7be6bee175cfcdcf280
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-06 16:31:29 -0300

    Objeto simples
    Sintaxe, propriedades e valores

---
+++ js/04-objetos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Exemplo 1: objeto com dados de uma pessoa
+const pessoa = {
+    // propriedade: valor
+    nome: "Chaves",
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP"
+};
+
+console.log(pessoa);
+
+// Acessamos as propriedades usando nomeDoObjeto.nomeDaPropriedade
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
+
+
