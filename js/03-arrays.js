1d4eee6d4dc8c7814bbeaa12fbb4d880173982a6
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-06 14:33:25 -0300

    Array com dados diversos/heterogêneos

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -13,4 +13,14 @@ console.log(vocalistas[0]);
 console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
 
 
+// Relembrando como criar/usar arrays (vetor)
+// No JS, vc pode colocar qualquer coisa em um array
+let seiLa = 6;
+const coisas = [10, "Senac", "<h2>Oie</h2>", seiLa, 15.88];
+console.log(coisas[1]);
+console.log(coisas[4]);
+
+
+
+
 
