5f7e204d03ea0bd3f1197bae855074645055bcb9
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-19 14:36:13 -0300

    Condicional SIMPLES

---
+++ js/05-condicionais.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Comandos Condicionais mais comuns:
+if      ->  SE
+else    ->  SENÃO 
+*/
+
+console.log("Exemplo 1");
+
+let numero = 50;
+
+// Condicional SIMPLES (usa apenas if)
+if(numero >= 10){
+    console.log("Se vc está vendo essa mensagem é porque a condição é VERDADEIRA!");
+}
