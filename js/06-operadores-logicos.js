14901c419e3ec0d5dd2b3308d218a54df8642977
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-19 16:52:29 -0300

    Sobre == e ===

--- js/06-operadores-logicos.js
+++ js/06-operadores-logicos.js
@@ -16,6 +16,15 @@ if(idade >= 18 && temCarta === true){
     console.log("Não pode dirigir");
 }
 
+/* Sobre duplo e triplo sinal de igual 
+- Ao usar == a comparação é somente do VALOR, ou seja, NÃO IMPORTA o tipo de dados (string, number, etc)
 
+- Ao usar === a comparação é do VALOR E DO TIPO DE DADO, ou seja, IMPORTA TUDO. */
+let a = 10;
+let b = '10'; 
+// let resultadoComparacao = a == b; // TRUE
+let resultadoComparacao = a === b; // FALSE
+
+console.log(resultadoComparacao);
 
 
