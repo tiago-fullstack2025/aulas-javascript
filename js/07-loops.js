5ddeaf62d52ac17986d056a5c4795125cc6d059b
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-20 14:11:32 -0300

    Loop WHILE

--- js/07-loops.js
+++ js/07-loops.js
@@ -6,3 +6,13 @@ São comandos que, baseados em alguma condição lógica, são programados para 
 Comandos mais tradicionais: while (ENQUANTO) e for (PARA)
 
 Normalmente, os loops são controlados através de uma variável contadora. */
+
+console.log("Exemplo 1: WHILE (enquanto)");
+
+let contador = 1;
+while(contador <= 5){
+    console.log("Valor do contador: "+contador);
+    contador++; // incremento
+    // contador = contador + 1;
+}
+
