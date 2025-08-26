eb88725e8925f82a7ccd8ffbd847f38a2827c0e3
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-26 15:18:07 -0300

    Função com return

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -53,6 +53,22 @@ saudacao("Mônica");
 saudacao("Catarina");
 saudacao(); // nesse caso, é usado "Visitante" como valor
 
+console.log("\nExemplo 5: função com parâmetros e retorno");
+
+/* Sempre que tivermos a necessidade de trabalhar
+com o resultado do processamento de uma função,
+então essa função deve ter o return. */
+function multiplicar(valor1, valor2){
+    return valor1 * valor2;
+}
+
+// Chamamos a função e recebemos o retorno/resultado dela
+let resultado1 = multiplicar(10, 5);
+let resultado2 = multiplicar(200, 10);
+
+// Exibimos o resultado
+console.log("Resultado 1: "+resultado1);
+console.log("Resultado 2: "+resultado2);
 
 
 
