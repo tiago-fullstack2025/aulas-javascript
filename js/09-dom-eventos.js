94b67a5e64f09b04d10d180ca30c41333f7e0919
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-26 17:26:02 -0300

    DOM e Eventos

---
+++ js/09-dom-eventos.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Exemplo 01 */
+
+// Procurando (querySelector) por um elemento chamado exemplo1
+const exemplo01 = document.querySelector("#exemplo1");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
+
+
