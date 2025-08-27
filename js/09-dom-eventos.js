74ae76ad7c436b89381f743f0f70db24ed0a01f0
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-08-27 14:02:11 -0300

    Exemplo querySelectorAll

--- js/09-dom-eventos.js
+++ js/09-dom-eventos.js
@@ -12,3 +12,8 @@ console.log(mensagem01);
 console.log(pagina);
 
 
+/* Para selecionar vários elementos, use querySelectorAll.
+O resultado é uma espécie de array de elementos (NodeList). */
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos);
+
